9b350d7a2a22ec6adccc8d3b72b61896d9110cbe
hilman kholik <hilmankholik.bisnis@gmail.com>
2022-08-01 00:00:16 +0700

    Adjust when post file request

--- request_send_file.py
+++ request_send_file.py
@@ -1,7 +1,11 @@
 import requests
 from datetime import datetime
 
-files = {'file': open('somefile.zip', 'rb')}
+filename= 'mov_bbb.mp4'
+filepath = '/base-path/service-upload-file/{}'.format(filename)
+files = {'file': (filename, open(filepath, 'rb'), 'text/xml'),
+         'Content-Disposition': 'form-data; name="file"; filename="' + filename + '"',
+         'Content-Type': 'text/xml'}
 r = requests.post('http://localhost:8188/', files=files)
 startt = datetime.now()
 try:
